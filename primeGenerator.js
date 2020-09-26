ae79a4b6305a5de9b4df667e86af4bd25d20f6d2
鈴木ちほり <suzukichihori@suzukichihoris-MacBook-Air.local>
2020-09-26 16:38:00 -0700

    second commit

--- primeGenerator.js
+++ primeGenerator.js
@@ -5,36 +5,31 @@ const primeGenerator = function(n) {
 
   let num = Math.sqrt(n);
   // let firstPrime = 0;
-  let list = [];
-  let newList = [];
   let primeList =[];
   let findList = [];
-  let firstNum = 0;
+  let p = 0;
   for (i = 2; i <= n; i++){
     findList.push(i);
   }
 
-  for(i = 0; primeList[i] >= num; i++){
-    firstNum = primeList[i];
-    for(j = i; j < findList.length; j++){
-      
-
+  while(p < num){
+    p = findList[0];
+    primeList.push(findList[0]);
+    for(i = 0; i < findList.length; i++){
+      if(findList[i] % p === 0){
+        findList.splice(i, 1);
+      }
     }
   }
-
-  // for (i = 0; i < list.length; i++){
-  //   for(j = 1; j < list.length; j++){
-  //     if (list[j] % list[i] === 0){
-  //       newList.splice(j, 1)
-  //     }
-  //   }
-  //   if(list[i] >= num){
-  //     return newList;
-  //   }
-  // }
+  let ansList = primeList.concat(findList);
+  return ansList;
 
 }
 
 console.log(primeGenerator(17));
 console.log(primeGenerator(3));
+console.log(primeGenerator(30));
+console.log(primeGenerator(120)); 
+//2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113
+
 
